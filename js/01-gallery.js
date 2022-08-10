b2d16df59a4c62b35aa90872891ce8b91f106489
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-10 19:18:36 +0300

    task1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryItemsRef = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src= '${item.preview}'
+      data-source='${item.original}'
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+gallery.innerHTML += galleryItemsRef;
+
+gallery.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src=${event.target.dataset.source}>
+`);
+  instance.show();
+
+  document.addEventListener("keydown", onGalleryEscape);
+
+  function onGalleryEscape(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
