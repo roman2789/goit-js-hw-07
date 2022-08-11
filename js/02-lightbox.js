af31765e141e2d9c08c34dc3d80569175ede870b
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-11 19:56:10 +0300

    second task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+let gallery = document.querySelector(".gallery");
+
+const galleryItemsRef = galleryItems
+  .map((item) => {
+    return `<a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src= '${item.preview}'
+      data-source='${item.original}'
+      alt="${item.description}"
+    />
+  </a>`;
+  })
+  .join("");
+
+gallery.innerHTML += galleryItemsRef;
+
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
